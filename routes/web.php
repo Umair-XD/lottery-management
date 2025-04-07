efc4bd34ffae0ce1b694846d09577caa80837faa
Umair-xdd <M.UmairWaheedAnsari@gmail.com>
2025-04-07 17:51:54 +0500

    Dark Mode Removed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -24,5 +25,7 @@ Route::middleware(['auth'
 ])->group(function () {
     Route::resource('tickets', TicketController::class);
 });
+
+Route::get('/users', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('users.index');
 
 require __DIR__.'/auth.php';
