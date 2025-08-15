2a6d6398c009238035da9c8eec16905ae361cb3d
Umair-xdd <m.umairwaheedansari@gmail.com>
2025-08-15 10:57:42 +0500

    Fixes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,3 @@
-// import './bootstrap'
-// import Alpine from 'alpinejs'
-// import carouselFactory from './carousel.js'
-
-// window.Alpine = Alpine
-
-// console.log('app.js loaded, Alpine', !!window.Alpine)
-// document.addEventListener('alpine:init', () => {
-//   console.log('alpine:init fired — store register next')
-//   Alpine.data('carousel', carouselFactory)
-
-//   Alpine.store('sidebar', {
-//     pinned: localStorage.getItem('sidebarPinned') === 'true' || false,
-//     toggle() {
-//       this.pinned = !this.pinned
-//       localStorage.setItem('sidebarPinned', this.pinned)
-//     }
-//   })
-// })
-
-// Alpine.start()
-
-
 import './bootstrap';
 import Alpine from 'alpinejs';
 import carouselFactory from './carousel.js';
