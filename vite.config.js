97d537ebb81eaedf74f11eff1d82fd28ae247b64
Umair-xdd <m.umairwaheedansari@gmail.com>
2025-08-21 21:48:59 +0500

    fixes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/swiper.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/css/swiper.css', 'resources/js/app.js', 'resources/js/tires.js', 'resources/js/products.js', 'resources/js/tickets.js'],
             refresh: true,
         }),
     ],
